375fd6935d6f77aa497f839ba551b8889a217d49
Koles_nl67 <iwashkalolesnikow@gmail.com>
2025-11-10 10:17:29 +0200

    test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,3 +1,16 @@
 def test_smoke():
     assert True
 
+from calculator import calculate_exspression
+
+def test_calculate_addition():
+    assert calculate_exspression('1 + 2 + 3') == '6'
+
+def test_calculate_subtraction():
+    assert calculate_exspression('2 - 3') == '-1'
+
+def test_smoke():
+    assert True
+
+def test_smoke1():
+    assert True
